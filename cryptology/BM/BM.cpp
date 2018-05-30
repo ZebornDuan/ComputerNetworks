3e4c4714abf80bea02208615ca6416037d2074ae
ZebornDuan <rebornduan@qq.com>
2018-05-30 16:49:36 +0800

    fix bugs

--- cryptology/BM/BM.cpp
+++ cryptology/BM/BM.cpp
@@ -2,54 +2,57 @@
 #include <cstdio>
 using namespace std;
 
-void BM(int s, int n) {
-	int l[15], m, d, c[15];
-	int n0;
-	c[0] = 1;
+void BM_LSRF(int *s, int n) {
+	int l[n], c[n][n], m, d = 0;
 	for (int i = 0; i < n; i++) {
-		if (s >> i & 1) {
-			n0 = i;
-			l[i + 1] = i + 1;
-			break;
-		} else {
-			l[i + 1] = 0;
-			c[i + 1] = 1;
-		}
+		l[i] = 0;
+		for (int j = 0; j < n; j++)
+			c[i][j] = 0;
 	}
-
-	c[n0 + 1] = 1 ^ (1 << (n0 + 1));
-	m = n0 - 1;
-
-	for (int i = n0 + 1; i < n; i++) {
-		d = s >> i & 1;
-		for (int j = 1; j < l[i]; j++)
-			d = d ^ ((c[i] >> j & 1) & (s >> (i - j) & 1));
+	c[0][0] = 1;
+	for (int i = 0; i < n; i++) {
+		d = 0;
+		for (int j = 0; j <= l[i]; j++)
+			d = d ^ (c[i][j] * s[i - j]);
 		if (d == 0) {
-			c[i + 1] = c[i];
 			l[i + 1] = l[i];
-		} else {
-			if (l[i] < i + 1 - l[i])
-				l[i + 1] = i + 1 - l[i];
-			else
-				l[i + 1] = l[i];
-			if (l[i] > l[i - 1])
-				m = i - 1;
-			c[i + 1] = c[i] ^ (c[m] << (i - m));
+			for (int h = 0; h < n; h++)
+				c[i + 1][h] = c[i][h];
 		}
+		else {
+			int k = i;
+			while (k >= 0 && l[k] == 0)
+				k--;
+			if (k == -1) {
+				for (int h = 0; h < n; h++)
+					c[i + 1][h] = c[i][h];
+				c[i + 1][i + 1] = 1;
+				l[i + 1] = i + 1;
+			} else {
+				k = i - 1;
+				while (l[k] == l[k + 1])
+					k--;
+				for (int h = 0; h < n; h++) 
+					if (h - (i - k) >= 0)
+						c[i + 1][h] = c[k][h - (i - k)] ^ c[i][h];
+					else
+						c[i + 1][h] = c[i][h];
+				l[i + 1] = l[i] > i + 1 - l[i] ? l[i] : i + 1 - l[i];
+			}
+		}
+		
 	}
 
 	cout << "f(x) = 1";
 	for (int j = 1; j < n; j++) {
-		if (c[n] >> j & 1)
+		if (c[n][j])
 			cout << " + x^" << j;
 	}
+	cout << endl << "l = " << l[n] << endl;
 }
 
 int main(int argc, char **argv) {
 	int s = 0;
 	int b[14] = {0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 1};
-	int length = 14;
-	for (int i = 0 ; i < length; i++) 
-		s = s ^ (b[i] << i);
-	BM(s, length);
+	BM_LSRF(b, 14);
 }
